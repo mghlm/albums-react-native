61ab2c5168c0b66ecf4518d3931ab47c368e268d
Magnus Holm <mail@magnusholm.com>
2017-05-23 19:15:49 +0100

    Add albumList and fetch data from API

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header.js'
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header.js';
+import AlbumList from './src/components/AlbumList.js';
 
 const App = () => (
-    <Header />
+  <View>
+    <Header headerText={'Albums'}/>
+    <AlbumList />
+  </View>
   );
 
 
---
+++ src/components/AlbumList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  render() {
+    console.log(this.state.albums)
+
+    return (
+      <View>
+        <Text> Album List </Text>
+      </View>
+    );
+  }
+};
+
+export default AlbumList;
