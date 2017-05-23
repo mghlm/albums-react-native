5c1b0a47a172e8b66021cbf52a0ff0ac92c33207
Magnus Holm <mail@magnusholm.com>
2017-05-23 16:54:39 +0100

    create header component

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header.js'
+
+const App = () => (
+    <Text> text </Text>
+  );
+
+
+AppRegistry.registerComponent('albums', () => App);
